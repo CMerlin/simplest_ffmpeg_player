53a5dd62e70d1c00ab8aef75a9cd83edd28958c5
CMerlin <1079843691@qq.com>
2017-07-02 13:18:13 +0800

    1.simplest_ffmpeg_decoder_pure此项目能正确的编译并执行

---
+++ simplest_ffmpeg_decoder_pure/compile_gcc.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+gcc simplest_ffmpeg_decoder_pure.cpp -I /usr/local/ffmpeg/include -L /usr/local/ffmpeg/lib -g -o simplest_ffmpeg_decoder_pure.out -I /usr/local/include -L /usr/local/lib \
+-lavcodec -lavutil -lswscale
